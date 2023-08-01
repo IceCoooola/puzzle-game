594f759de4a1a9844f2ea3c0274e70d0f159750b
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-01 01:19:52 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import cola.ice.GameJframe;
+import cola.ice.LoginJframe;
+import cola.ice.RegisterJframe;
+
+public class Main {
+    public static void main(String[] args) {
+        GameJframe j = new GameJframe();
+
+        // LoginJframe l = new LoginJframe();
+        // RegisterJframe r = new RegisterJframe();
+    }
+}
