2145d214c31bcfa65f21f7ed2d7991c2529e501d
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-02 15:48:05 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,10 @@ import cola.ice.GameJframe;
 import cola.ice.LoginJframe;
 import cola.ice.RegisterJframe;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        GameJframe j = new GameJframe();
-
-        // LoginJframe l = new LoginJframe();
-        // RegisterJframe r = new RegisterJframe();
+        LoginJframe l = new LoginJframe();
     }
 }
